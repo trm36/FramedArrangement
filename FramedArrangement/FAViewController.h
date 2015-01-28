f6e211ec69ba298f70df5552f1bfb8af60c3e125
Taylor Mott <trm36@humboldt.edu>
2015-01-28 14:44:23 -0700

    STEP 1: Create a view controller subclass
    - Clone down this project (or create a new single view controller in Xcode)
    - Add a view controller to the app (if you cloned down)
    - Set a new instance of the view controller to the root view controller of the window
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ FramedArrangement/FAViewController.h
@@ -0,0 +1,13 @@
+//
+//  FAViewController.h
+//  FramedArrangement
+//
+//  Created by Taylor Mott on 28.1.2015.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FAViewController : UIViewController
+
+@end
