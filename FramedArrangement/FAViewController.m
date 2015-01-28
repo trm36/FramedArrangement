a578447c8fd571251f0259d35a65fc191e355449
Taylor Mott <trm36@humboldt.edu>
2015-01-28 15:15:32 -0700

    STEP 2: Add the views to the view controller
    - Add a redView, blueView, greenView, yellowView property to the interface of the view controller
    - Add each view to the view controller's main view in the viewDidLoad method
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- FramedArrangement/FAViewController.m
+++ FramedArrangement/FAViewController.m
@@ -10,6 +10,11 @@
 
 @interface FAViewController ()
 
+@property (strong, nonatomic) UIView *redView;
+@property (strong, nonatomic) UIView *blueView;
+@property (strong, nonatomic) UIView *greenView;
+@property (strong, nonatomic) UIView *yellowView;
+
 @end
 
 @implementation FAViewController
@@ -17,6 +22,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.redView = [UIView new];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
+    
+    self.blueView = [[UIView alloc] init];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.blueView];
+    
+    self.greenView = [UIView new];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.greenView];
+    
+    self.yellowView = [UIView new];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.yellowView];
 }
 
 - (void)didReceiveMemoryWarning {
