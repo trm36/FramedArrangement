4a1cc5b77d204eedea4ad406d152372d1aff3262
Taylor Mott <trm36@humboldt.edu>
2015-01-28 21:28:56 -0700

    Removes comments
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- FramedArrangement/FAViewController.m
+++ FramedArrangement/FAViewController.m
@@ -21,7 +21,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     self.redView = [UIView new];
     self.redView.backgroundColor = [UIColor redColor];
@@ -46,7 +45,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)layoutSquares
@@ -101,15 +99,5 @@
 {
     [self layoutSquares];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
